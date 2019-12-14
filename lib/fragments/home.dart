abe68d4ae317e40a70dfe336c10095610b2210b9
brianrizqi <rizqibrian27@gmail.com>
2019-12-14 17:17:08 +0700

    WIP : artikel

--- lib/fragments/home.dart
+++ lib/fragments/home.dart
@@ -8,7 +8,6 @@ class Home extends StatelessWidget {
     const afternoon = Color(0xFFf9ed6a);
     const night = Color(0xFF284b69);
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-      systemNavigationBarColor: afternoon, // navigation bar color
       statusBarColor: afternoon, // status bar color
       statusBarIconBrightness: Brightness.dark, // status bar icons' color
       systemNavigationBarIconBrightness:
@@ -17,57 +16,102 @@ class Home extends StatelessWidget {
     return new Scaffold(
       backgroundColor: Colors.white,
       resizeToAvoidBottomPadding: false,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Container(
-            child: Stack(
-              children: <Widget>[
-                Container(
-                  color: afternoon,
-                  width: double.maxFinite,
-                  alignment: Alignment.center,
-                  child: Text(
-                    'Selamat Siang',
-                    style: TextStyle(
-                        fontSize: 14.0,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                    fontFamily: 'Source-sans Pro'),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(),
+            Container(
+              child: Stack(
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.only(top: 30.0),
+                    color: afternoon,
+                    width: double.maxFinite,
+                    alignment: Alignment.center,
+                    child: Text(
+                      'Selamat Siang',
+                      style: TextStyle(
+                          fontSize: 14.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontFamily: 'Source-sans Pro'),
+                    ),
                   ),
-                  margin: const EdgeInsets.only(top: 25.0),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          Container(
-            child: Stack(
-              children: <Widget>[
-                Container(
-                  decoration: BoxDecoration(
-                      image: DecorationImage(
-                          image: AssetImage('assets/images/afternoon.png'),
-                          fit: BoxFit.fill)),
-                  height: 130,
-                ),
-              ],
-            ),
-          ),
-          Container(
-            child: Stack(
-              children: <Widget>[
-                Container(
-                  padding: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 10.0),
-                  child: Text(
-                    'Article',
-                    style:
-                        TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+            Container(
+              child: Stack(
+                children: <Widget>[
+                  Container(
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage('assets/images/afternoon.png'),
+                            fit: BoxFit.fill)),
+                    height: 130,
                   ),
-                ),
+                ],
+              ),
+            ),
+            Container(
+              child: Stack(
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 0.0),
+                    child: Text(
+                      'Article',
+                      style: TextStyle(
+                          fontSize: 20.0, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Column(
+              children: <Widget>[
+                SizedBox(
+                  child: new ListView(
+                    padding: EdgeInsets.only(bottom: 20),
+                    shrinkWrap: true,
+                    children: <Widget>[
+                      new Artikel(
+                        "https://st2.depositphotos.com/1006318/5909/v/950/depositphotos_59095529-stock-illustration-profile-icon-male-avatar.jpg",
+                        "asd",
+                      ),
+                      new Artikel(
+                        "https://st2.depositphotos.com/1006318/5909/v/950/depositphotos_59095529-stock-illustration-profile-icon-male-avatar.jpg",
+                        "asd",
+                      ),
+                    ],
+                  ),
+                )
               ],
             ),
-          )
-        ],
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Artikel extends StatelessWidget {
+  Artikel(this.gambar, this.judul);
+
+  final String gambar;
+  final String judul;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      padding: EdgeInsets.all(10),
+      child: new Center(
+        child: new Row(
+          children: <Widget>[
+            new Image(image: NetworkImage(gambar), width: 100),
+            new Text(judul)
+          ],
+        ),
       ),
     );
   }
