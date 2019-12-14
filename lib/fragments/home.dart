742889873d825b287a6367fc808e60e134c37556
brianrizqi <rizqibrian27@gmail.com>
2019-12-14 12:47:07 +0700

    WIP : Dashboard

--- lib/fragments/home.dart
+++ lib/fragments/home.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    const morning = Color(0xFFc0eae6);
+    const afternoon = Color(0xFFf9ed6a);
+    const night = Color(0xFF284b69);
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      systemNavigationBarColor: afternoon, // navigation bar color
+      statusBarColor: afternoon, // status bar color
+      statusBarIconBrightness: Brightness.dark, // status bar icons' color
+      systemNavigationBarIconBrightness:
+          Brightness.dark, //navigation bar icons' color
+    ));
     return new Scaffold(
       backgroundColor: Colors.white,
       resizeToAvoidBottomPadding: false,
@@ -13,28 +24,49 @@ class Home extends StatelessWidget {
             child: Stack(
               children: <Widget>[
                 Container(
-                  decoration: BoxDecoration(
-                      image: DecorationImage(
-                          image: AssetImage('assets/images/morning.png'),
-                          fit: BoxFit.fill)),
-                  height: 130,
-                  margin: const EdgeInsets.only(top: 20.0),
+                  color: afternoon,
+                  width: double.maxFinite,
+                  alignment: Alignment.center,
+                  child: Text(
+                    'Selamat Siang',
+                    style: TextStyle(
+                        fontSize: 14.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                    fontFamily: 'Source-sans Pro'),
+                  ),
+                  margin: const EdgeInsets.only(top: 25.0),
                 ),
               ],
             ),
           ),
           Container(
-              child: Stack(
-            children: <Widget>[
-              Container(
-                padding: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 10.0),
-                child: Text(
-                  'Artikel',
-                  style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+            child: Stack(
+              children: <Widget>[
+                Container(
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage('assets/images/afternoon.png'),
+                          fit: BoxFit.fill)),
+                  height: 130,
                 ),
-              ),
-            ],
-          ))
+              ],
+            ),
+          ),
+          Container(
+            child: Stack(
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 10.0),
+                  child: Text(
+                    'Article',
+                    style:
+                        TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ],
+            ),
+          )
         ],
       ),
     );
